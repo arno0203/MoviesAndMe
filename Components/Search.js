2c002adb76122ae41335140bf9f1f1bda8d7842f
Ado <arnaud.dollois@voisinsvigilants.org>
2019-10-04 09:18:44 +0200

    Add Navigation

--- Components/Search.js
+++ Components/Search.js
@@ -6,110 +6,108 @@ import FilmItem from './FilmItem'
 import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi'
 
 class Search extends React.Component {
-  constructor(props){
-    super(props)
-    this.searchedText = ""
-    this.page = 0
-    this.totalPage = 0
-    this.state = {
-      films : [],
-      isLoading: false
-    }
-  }
 
-  _loadFilms(){
-    if(this.searchedText.length > 0){
-      this.setState({isLoading: true})
-    getFilmsFromApiWithSearchedText(this.searchedText, this.page+1).then(
-        data => {
-          this.page = data.page
-          this.totalPage = data.total_pages
-          this.setState({
-            films:  [ ...this.state.films, ...data.results ] // => films: this.state.films.concat(data.results)
-            , isLoading: false
-          })
+    constructor(props) {
+        super(props)
+        this.searchedText = ""
+        this.page = 0
+        this.totalPages = 0
+        this.state = {
+            films: [],
+            isLoading: false
         }
-      )
     }
-  } 
 
-  _searchTextInputChanged(text) {
-       this.searchedText = text
-   }
+    _loadFilms() {
+        if (this.searchedText.length > 0) {
+            this.setState({ isLoading: true })
+            getFilmsFromApiWithSearchedText(this.searchedText, this.page+1).then(data => {
+                this.page = data.page
+                this.totalPages = data.total_pages
+                this.setState({
+                    films: [ ...this.state.films, ...data.results ],
+                    isLoading: false
+                })
+            })
+        }
+    }
 
-  _searchFilm(){
-    this.page = 0
-    this.totalPage = 0
-    
-    this.setState(
-      {
-        films : []
-      }
-      , () => {
-        console.log("--->Page : " + this.page + " / TotalPages : " + this.totalPages + " / Nombre de films : " + this.state.films.length)
-        this._loadFilms()
-      })
-   
-   }
+    _searchTextInputChanged(text) {
+        this.searchedText = text
+    }
 
-  _displayLoading() {
-     if (this.state.isLoading) {
-       return (
-         <View style={styles.loading_container}>
-           <ActivityIndicator size='large' />
-         </View>
-       )
-     }
-  }
+    _searchFilms() {
+        this.page = 0
+        this.totalPages = 0
+        this.setState({
+            films: [],
+        }, () => {
+            this._loadFilms()
+        })
+    }
 
-  render() {
-    return (
-      <View style={styles.main_container}>
-        <TextInput
-          style={styles.textinput}
-          placeholder='Titre du film'
-          onChangeText={(text) => this._searchTextInputChanged(text)}
-          onSubmitEditing= {() => this._loadFilms()}
-          />
-        <Button title='Rechercher' onPress={() => this._searchFilm()}/>
-        <FlatList
-          data={this.state.films}
-          keyExtractor={(item) => item.id.toString()}
-          renderItem={({item}) => <FilmItem film={item}/>}
-          onEndReached={() => {
-            if(this.page < this.totalPage){
-              this._loadFilms()
-            }
-          }}
-        />
-        {this._displayLoading()}
-      </View>
-    )
-  }
+    _displayLoading() {
+        if (this.state.isLoading) {
+            return (
+                <View style={styles.loading_container}>
+                    <ActivityIndicator size='large' />
+                </View>
+            )
+        }
+    }
+
+    _displayDetailForFilm = (idFilm) => {
+        console.log("Display film with id "+idFilm)
+    }
+
+    render() {
+        return (
+            <View style={styles.main_container}>
+                <TextInput
+                    style={styles.textinput}
+                    placeholder='Titre du film'
+                    onChangeText={(text) => this._searchTextInputChanged(text)}
+                    onSubmitEditing={() => this._searchFilms()}
+                />
+                <Button title='Rechercher' onPress={() => this._searchFilms()}/>
+                <FlatList
+                    data={this.state.films}
+                    keyExtractor={(item) => item.id.toString()}
+                    renderItem={({item}) => <FilmItem film={item} displayDetailForFilm={this._displayDetailForFilm}/>}
+                    onEndReachedThreshold={0.5}
+                    onEndReached={() => {
+                        if (this.page < this.totalPages) {
+                            this._loadFilms()
+                        }
+                    }}
+                />
+                {this._displayLoading()}
+            </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
-  main_container: {
-    flex: 1,
-    marginTop: 20
-  },
-  textinput: {
-    marginLeft: 5,
-    marginRight: 5,
-    height: 50,
-    borderColor: '#000000',
-    borderWidth: 1,
-    paddingLeft: 5
-  },
-  loading_container: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    top: 100,
-    bottom: 0,
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
+    main_container: {
+        flex: 1
+    },
+    textinput: {
+        marginLeft: 5,
+        marginRight: 5,
+        height: 50,
+        borderColor: '#000000',
+        borderWidth: 1,
+        paddingLeft: 5
+    },
+    loading_container: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 100,
+        bottom: 0,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
 })
 
 export default Search
